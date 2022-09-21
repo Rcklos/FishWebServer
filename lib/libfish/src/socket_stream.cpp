4de93fc90ddd9c71ff32882a3a1aca88c3f94338
rcklos <2362819691@qq.com>
2022-09-22 00:00:03 +0800

    add stream

---
+++ lib/libfish/src/socket_stream.cpp
@@ -0,0 +1,59 @@
+﻿#include "socket_stream.h"
+
+FISH_NAMESPACE_START
+SocketStream::SocketStream(Socket::Ptr socket, bool auto_close)
+    : socket_(socket), auto_close_(auto_close){
+}
+
+SocketStream::~SocketStream() {
+    if (auto_close_ && socket_) {
+        socket_->Close();
+    }
+}
+
+int SocketStream::Read(void *buffer, size_t length) {
+    if (!IsConnected() || !buffer) {
+        return -1;
+    }
+    return socket_->Recv((char*)buffer, length);
+}
+
+int SocketStream::Read(ByteArray::Ptr ba, size_t length) {
+    if (!IsConnected() || !ba) {
+        return -1;
+    }
+    std::vector<iovec> iovs;
+    ba->GetWriteIovecs(iovs, length);
+    int ret = socket_->Recv(&iovs[0], iovs.size());
+    ba->AddWritePos(ret);
+    return ret;
+}
+
+int SocketStream::Write(const void *buffer, size_t length) {
+    if (!IsConnected() || !buffer) {
+        return -1;
+    }
+    return socket_->Send((const char*)buffer, length);
+}
+
+int SocketStream::Write(ByteArray::Ptr ba, size_t length) {
+    if (!ba) {
+        return -1;
+    }
+    std::vector<iovec> iovs;
+    ba->GetReadIovecs(iovs, length);
+    int ret = socket_->Send(&iovs[0], iovs.size());
+    ba->AddReadPos(ret);
+    return ret;
+}
+
+void SocketStream::Close() {
+    if (socket_) {
+        socket_->Close();
+    }
+}
+
+bool SocketStream::IsConnected() const {
+    return socket_ && socket_->IsConnected();
+}
+FISH_NAMESPACE_END
---
+++ lib/libfish/src/socket_stream.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "stream.h"
+#include "socket.h"
+#include "common.h"
+
+FISH_NAMESPACE_START
+
+class SocketStream : public Stream, public BaseDump{
+public:
+    typedef std::shared_ptr<SocketStream> Ptr;
+
+public:
+    SocketStream(Socket::Ptr socket, bool auto_close = false);
+    ~SocketStream();
+
+public:
+    int Read(void *buffer, size_t length) override;
+
+    int Read(ByteArray::Ptr ba, size_t length) override;
+
+    int Write(const void *buffer, size_t length) override;
+
+    int Write(ByteArray::Ptr ba, size_t length) override;
+
+    void Close() override;
+
+    bool IsConnected() const;
+
+private:
+    Socket::Ptr socket_;
+    bool auto_close_;
+
+public:
+    FUNCTION_BUILDER_VAR_GETTER(Socket, socket_);
+};
+
+FISH_NAMESPACE_END
+
