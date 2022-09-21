13b48dcb342f6eba5ebb9fc187f2dd3c96fe4970
rcklos <2362819691@qq.com>
2022-09-21 13:42:40 +0800

    add tcp server

---
+++ lib/libfish/src/tcp_server.cpp
@@ -0,0 +1,169 @@
+﻿#include "tcp_server.h"
+
+FISH_NAMESPACE_START
+
+static int64_t g_tcp_server_accept_timeout = 2 * 1000;
+
+TcpServer::TcpServer(const std::string& name)
+    : name_(name)
+    , io_manager_(nullptr)
+    , accept_timeout_(g_tcp_server_accept_timeout){
+}
+
+TcpServer::Ptr TcpServer::Create(const std::string &name) {
+    return TcpServer::Ptr(new TcpServer(name));
+}
+
+TcpServer::~TcpServer() {
+    for (const auto& it : sockets_) {
+        it->Close();
+    }
+}
+
+void TcpServer::Dump(std::ostream &os) const {
+    if (!is_init_) {
+        os << "TcpServer no init";
+        return;
+    }
+    os << "TcpServer{" << EX_STRING_VARS(type_, name_, ssl_, accept_timeout_);
+    os << "\"sockets\":[";
+    for (auto& socket : sockets_) {
+        os << "{" << "socket:" << *socket << "},";
+    }
+    os << "]}";
+}
+
+bool TcpServer::Init(Address::Ptr address, IoManager* io_manager, uint32_t client_handler_cnt) {
+    if (is_init_) {
+        return false;
+    }
+    if (!Bind({address})) {
+        return false;
+    }
+    if (!io_manager) {
+        return false;
+    }
+    io_manager_ = io_manager;
+    client_handler_cnt_ = client_handler_cnt;
+    is_init_ = true;
+    return true;
+}
+
+bool TcpServer::Init(Socket::Ptr socket, IoManager *io_manager, uint32_t client_handler_cnt) {
+    if (is_init_) {
+        return false;
+    }
+    if (!socket || !socket->IsValid()) {
+        return false;
+    }
+    sockets_.push_back(socket);
+    if (!io_manager) {
+        return false;
+    }
+    io_manager_ = io_manager;
+    client_handler_cnt_ = client_handler_cnt;
+    is_init_ = true;
+    return true;
+}
+
+bool TcpServer::Bind(const std::vector<Address::Ptr>& addrs/*, bool ssl*/) {
+    for (const auto& addr : addrs) {
+        auto sock = Socket::CreateTCP(addr);
+        if (!sock) {
+            goto fail;
+        }
+        if (!sock->Init()) {
+            goto fail;
+        }
+        if (!sock->Bind(addr)) {
+            goto fail;
+        }
+        if (!sock->Listen(128)) {
+            goto fail;
+        }
+        sockets_.push_back(sock);
+    }
+    return true;
+fail:
+    sockets_.clear();
+    return false;
+}
+
+bool TcpServer::Start() {
+    if (!is_init_) {
+        return false;
+    }
+    if (!GetIsStop()) {
+        return true;
+    }
+    SetIsStop(false);
+
+    for (const auto& socket : sockets_) {
+        io_manager_->Schedule(Coroutine::Create(std::bind(&TcpServer::OnAccept, shared_from_this(), socket)));
+    }
+
+    for (uint32_t i = 0; i < client_handler_cnt_; ++i) {
+        io_manager_->Schedule(Coroutine::Create(std::bind(&TcpServer::OnClientHandle, shared_from_this(), nullptr)));
+    }
+
+    return true;
+}
+
+void TcpServer::Stop() {
+    if (!is_init_) {
+        return;
+    }
+    SetIsStop(true);
+    io_manager_->Schedule(Coroutine::Create([this]() {
+        for (const auto &socket: sockets_) {
+            socket->Close();
+        }
+        sockets_.clear();
+    }));
+}
+
+void TcpServer::ClientHandle(Socket::Ptr client) {
+    if (!client) {
+        return ;
+    }
+    std::string recv_buf;
+    while (client->Recv(recv_buf) > 0) {
+        if (recv_buf == "0000") {
+            Stop();
+            break;
+        }
+    }
+}
+
+void TcpServer::OnAccept(Socket::Ptr listen_socket) {
+    if (accept_timeout_ != -1) {
+        listen_socket->SetRecvTimeOut(g_tcp_server_accept_timeout);
+    }
+    while(!GetIsStop()) {
+        while (idle_cos_.empty()) {
+            usleep(500);
+        }
+        auto client = listen_socket->Accept();
+        if (!client) {
+            continue;
+        }
+        auto t = idle_cos_.front();
+        idle_cos_.pop();
+        t->cli = client;
+        t->co->Resume();
+    }
+}
+
+void TcpServer::OnClientHandle(Socket::Ptr client) {
+    while(!GetIsStop()) {
+        while (!client) {
+            idle_cos_.push(std::make_shared<Task>(Coroutine::GetCurCoroutine(), client));
+            Coroutine::Yield();
+        }
+        ClientHandle(client);
+        client->Close();
+        client = nullptr;
+    }
+}
+
+FISH_NAMESPACE_END
---
+++ lib/libfish/src/tcp_server.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+
+#include <memory>
+#include <vector>
+#include <map>
+#include "iomanager.h"
+#include "socket.h"
+#include "address.h"
+#include "common.h"
+
+FISH_NAMESPACE_START
+
+class TcpServer : public std::enable_shared_from_this<TcpServer>,
+                  Noncopyable,
+                  public BaseDump{
+public:
+    typedef std::shared_ptr<TcpServer> Ptr;
+
+    struct Task {
+        typedef std::shared_ptr<Task> Ptr;
+        Task(Coroutine::Ptr c, Socket::Ptr& cl) : co(c), cli(cl){}
+        Coroutine::Ptr      co  = nullptr;
+        Socket::Ptr&        cli;
+    };
+
+protected:
+    /**
+     * @brief 构造函数 (不开放构造，防止share_from_this()访问失败)
+     * @param[in] worker: socket客户端工作的协程调度器
+     * @param[in] io_worder: todo
+     * @param[in] accept_worker: 服务器接受连接的协程调度器
+     * @return
+     */
+    TcpServer(const std::string& name = "none");
+
+public:
+    static TcpServer::Ptr Create(const std::string& name = "none");
+
+    /**
+     * @brief 析构函数
+     */
+    virtual ~TcpServer();
+
+public:
+    void Dump(std::ostream &os) const override;
+
+public:
+    virtual bool Init(Address::Ptr address, IoManager* io_manager = IoManager::GetCurIoManager(), uint32_t client_handler_cnt = 512/*, bool ssl = false*/);
+    virtual bool Init(Socket::Ptr socket, IoManager* io_manager = IoManager::GetCurIoManager(), uint32_t client_handler_cnt = 512/*, bool ssl = false*/);
+    virtual bool Bind(const std::vector<Address::Ptr>& addrs/*, BOOL SSL = FALSE*/);
+    virtual bool Start();
+    virtual void Stop();
+
+    virtual void ClientHandle(Socket::Ptr client);
+    virtual void OnAccept(Socket::Ptr listen_socket);
+
+public:
+    size_t GetSocketNum(){ return sockets_.size(); }
+
+private:
+    void OnClientHandle(Socket::Ptr client);
+
+private:
+    std::string                 name_                   = "none";
+    IoManager*                  io_manager_             = nullptr;
+    int64_t                     accept_timeout_         = -1;
+    uint32_t                    client_handler_cnt_     = 512;
+    std::string                 type_                   = "tcp";
+    bool                        is_stop_                = true;
+    bool                        ssl_                    = false;
+    bool                        is_init_                = false;
+    std::queue<Task::Ptr>       idle_cos_;
+    std::vector<Socket::Ptr>    sockets_;
+
+public:
+    FUNCTION_BUILDER_VAR(IsStop, is_stop_);
+};
+FISH_NAMESPACE_END
